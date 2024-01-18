dab8956dfdb87139f0d93103e5bd4bcb536b8ab3
Madison Reynolds <madison.reynolds@phoenix.edu>
2024-01-17 17:48:10 -0700

    added code to database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,10 +18,10 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ jate: content });
+  const request = store.put({id: 1, value: content});
   const result = await request;
   console.log('Data saved to the database', result);  
-  console.error('putDb not implemented');
+;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -30,10 +30,9 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  console.error('getDb not implemented');
   return result;
 };
 
